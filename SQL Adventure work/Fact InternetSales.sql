5fa525b7c8ffb8692c45191ae17b240ab41f6266
Ali mohamed tawfeek <119808972+alimohamed99@users.noreply.github.com>
2022-12-08 10:52:55 +0200

    Add files via upload

---
+++ SQL Adventure work/Fact InternetSales.sql
@@ -0,0 +1,14 @@
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 
+ORDER BY
+  OrderDateKey ASC
